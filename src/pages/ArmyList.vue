2b5704a6b9f61895866af723b5fb483a61551958
WryanCartie <wryancartie@gmail.com>
2023-03-30 11:42:13 +0800

    Make the army list link span the entire width.

--- src/pages/ArmyList.vue
+++ src/pages/ArmyList.vue
@@ -4,7 +4,7 @@
     <h1 class="text-5xl border border-black px-2 bg-black text-white w-100 h-[110px] cursor-pointer"> Army List</h1>
     <ul>
         <li class="hover:bg-green-700 active:bg-green-600 hover:text-white focus:bg-green-500  text-medium font-medium mx-2 my-1  px-2.5 py-0.5 rounded" v-for="state in CCStateList" :key="state">
-            <router-link :to="'/armies/'+stateList[state]">{{ state }}</router-link>
+            <router-link class="inline-block w-full" :to="'/armies/'+stateList[state]">{{ state }}</router-link>
         </li>
     </ul>
    
