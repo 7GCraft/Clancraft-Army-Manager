78738fcfa0ad432fc331073a956d9d09c1e6f431
WryanCartie <33276404+WryanCartie@users.noreply.github.com>
2023-07-15 21:49:49 +0800

    Add WIP basic excel import in the add state.

--- frontend/src/components/AddStateModal.vue
+++ frontend/src/components/AddStateModal.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="fixed z-10 inset-0 overflow-y-auto" v-if="show">
+  <div class="fixed inset-0 z-10 overflow-y-auto" v-if="show">
     <div
-      class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+      class="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0"
     >
       <div class="fixed inset-0 transition-opacity">
         <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
@@ -11,24 +11,24 @@
       >&#8203;
 
       <div
-        class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+        class="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
         role="dialog"
         aria-modal="true"
         aria-labelledby="modal-headline"
       >
-        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div class="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
           <div class="sm:flex sm:items-start">
             <div
-              class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-400 sm:mx-0 sm:h-10 sm:w-10"
+              class="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-blue-400 rounded-full sm:mx-0 sm:h-10 sm:w-10"
             >
               <font-awesome-icon
                 icon="fa-solid fa-plus"
-                class="text-white text-3xl hover:animate-pulse"
+                class="text-3xl text-white hover:animate-pulse"
               ></font-awesome-icon>
             </div>
             <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
               <h3
-                class="text-lg leading-6 font-medium text-gray-900"
+                class="text-lg font-medium leading-6 text-gray-900"
                 id="modal-headline"
               >
                 Add State Modal
@@ -38,35 +38,35 @@
                   @submit="$emit('submitModal', stateData)"
                   id="add-state-form"
                 >
-                  <div class="form-control mt-1">
+                  <div class="mt-1 form-control">
                     <label for="state-name" class="mr-[52px]">State Name</label>
                     <input
                       v-model="stateData.name"
                       type="text"
-                      class="border border-black p-1"
+                      class="p-1 border border-black"
                     />
                   </div>
-                  <div class="form-control mt-1">
+                  <div class="mt-1 form-control">
                     <label for="state-name" class="mr-20">State ID</label>
                     <input
                       v-model="stateData.id"
                       type="text"
-                      class="border border-black p-1"
+                      class="p-1 border border-black"
                     />
                   </div>
-                  <div class="form-control mt-1">
+                  <div class="mt-1 form-control">
                     <label for="state-name" class="mr-8">State Currency</label>
                     <input
                       v-model="stateData.currency"
                       type="text"
-                      class="border-black border p-1"
+                      class="p-1 border border-black"
                     />
                   </div>
-                  <div class="form-control mt-1">
+                  <div class="mt-1 form-control">
                     <label for="state-name" class="mr-[64px]">State Map</label>
                     <select
                       v-model="stateData.map"
-                      class="border border-black p-1 w-48"
+                      class="w-48 p-1 border border-black"
                     >
                       <option
                         :key="key"
@@ -76,6 +76,7 @@
                         {{ value }}
                       </option>
                     </select>
+                    <input type="file" @change="importExcel"/>
                   </div>
                 </form>
               </div>
@@ -83,18 +84,18 @@
           </div>
         </div>
         <div
-          class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row justify-end"
+          class="justify-end px-4 py-3 bg-gray-50 sm:px-6 sm:flex sm:flex-row"
         >
           <button
             type="submit"
-            class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue sm:ml-3 sm:w-auto sm:text-sm"
+            class="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue sm:ml-3 sm:w-auto sm:text-sm"
             form="add-state-form"
           >
             Submit
           </button>
           <button
             type="button"
-            class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-gray-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray sm:ml-3 sm:w-auto sm:text-sm"
+            class="inline-flex justify-center w-full px-4 py-2 text-base font-medium text-white bg-gray-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray sm:ml-3 sm:w-auto sm:text-sm"
             @click="$emit('closeModal')"
           >
             Close
@@ -106,6 +107,7 @@
 </template>
 
 <script>
+import * as XLSX from 'xlsx';
 export default {
   props: ['show'],
   emits: ['closeModal', 'submitModal'],
@@ -131,5 +133,34 @@ export default {
       },
     };
   },
+  methods: {
+    importExcel(event){
+      const file = event.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(sheet);
+        const tableData = jsonData.map((row) => {
+          const newObj = {};
+          Object.keys(row).forEach((key) => {
+            console.log(row,'row',key,'key')
+            const header = key;
+            const value = row[key];
+            newObj[header] = value;
+          });
+          return newObj;
+        });
+
+        // Assign the resulting JSON array to a data property or emit it as an event
+        console.log(tableData)
+      };
+
+      reader.readAsArrayBuffer(file);
+    },
+  }
 };
 </script>
