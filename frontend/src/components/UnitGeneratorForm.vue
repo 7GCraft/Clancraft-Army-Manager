8379c31610c13ef1622373a5e07776152dbe4a57
WryanCartie <wryancartie@gmail.com>
2024-12-24 00:12:41 +0800

    Clean up console.logs in development of unit generator

--- frontend/src/components/UnitGeneratorForm.vue
+++ frontend/src/components/UnitGeneratorForm.vue
@@ -151,7 +151,6 @@ export default {
         const nobleSize = Math.round((this.generationSize * 2) / 10);
         const levySize = Math.round((this.generationSize * 8) / 10);
         const nobleUnits = this.units.filter(unit => {
-          console.log(unit.Tier.toLowerCase(),"tapejara")
           return (
             unit.Tier.toLowerCase().includes('cavalry')||
             unit.Tier.toLowerCase().includes('tier ii')||
